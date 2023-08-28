4c9ff8d55e9044b59c5b85001b219009386164e3
Administrator <Administrator@DESKTOP-R9PAFGC>
2023-08-28 21:05:31 +0900

    first commit

---
+++ duplicateInteger/src/duplicateInteger/DuplicateInteger3.java
@@ -0,0 +1,30 @@
+package duplicateInteger;
+
+import java.util.Scanner;
+
+public class DuplicateInteger3 {
+	public static void main(String[] args) {
+		//중복되는 정수를 배열로 출력하기
+	Scanner scan = new Scanner(System.in);
+	String inner = scan.nextLine();
+	String[] input = inner.split(",");      //배열선언
+
+//    int w =input.length-1;
+//    System.out.println(input[0]+","+input[w]);
+//    if( input[0].equals(input[w]) ) {
+//		System.out.println(input[0] +"," + input[w]);
+//    }
+    for( int i = 0; i <= input.length-1; i++) {
+    	for( int n = 0; n <= input.length-1; n++) {
+    		if( input[i].equals(input[n]) ) {
+    			if(input[i] == input[n]) {
+    				break;
+    			}
+    			System.out.println(input[i] +"," + input[n]);
+    	}
+    	}
+    }
+    
+    scan.close();
+	}
+}
